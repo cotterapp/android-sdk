b2a9a67ab34df3081d68f73bb35b66465df9e585
Robert <roberto.capah@gmail.com>
2020-09-24 10:42:14 +0700

    (Enchancement) Handle request from android devices API < 21 to server with security TLS 1.1 and 1.2

--- app/src/main/java/com/cotter/app/VolleySingleton.java
+++ app/src/main/java/com/cotter/app/VolleySingleton.java
@@ -2,12 +2,23 @@ package com.cotter.app;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.Log;
 import android.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.security.ProviderInstaller;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 public class VolleySingleton {
     private static VolleySingleton instance;
@@ -47,7 +58,42 @@ public class VolleySingleton {
         if (requestQueue == null) {
             // getApplicationContext() is key, it keeps you from leaking the
             // Activity or BroadcastReceiver if someone passes one in.
-            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                    && Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+                try {
+                    ProviderInstaller.installIfNeeded(ctx.getApplicationContext());
+                } catch (GooglePlayServicesRepairableException e) {
+                    // Indicates that Google Play services is out of date, disabled, etc.
+                    // Prompt the user to install/update/enable Google Play services.
+                    GooglePlayServicesUtil.showErrorNotification(e.getConnectionStatusCode(), ctx.getApplicationContext());
+                    // Notify the SyncManager that a soft error occurred.
+//                    syncResult.stats.numIOExceptions++;
+//                    return;
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // Indicates a non-recoverable error; the ProviderInstaller is not able
+                    // to install an up-to-date Provider.
+                    // Notify the SyncManager that a hard error occurred.
+//                    syncResult.stats.numAuthExceptions++;
+//                    return;
+                }
+
+                HttpStack stack = null;
+                try {
+                    stack = new HurlStack(null, new TLSSocketFactory());
+                } catch (KeyManagementException e) {
+                    e.printStackTrace();
+                    Log.d("Your Wrapper Class", "Could not create new stack for TLS v1.2");
+                    stack = new HurlStack();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                    Log.d("Your Wrapper Class", "Could not create new stack for TLS v1.2");
+                    stack = new HurlStack();
+                }
+                requestQueue = Volley.newRequestQueue(ctx.getApplicationContext(), stack);
+            } else {
+                requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+            }
+//            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
         }
         return requestQueue;
     }
